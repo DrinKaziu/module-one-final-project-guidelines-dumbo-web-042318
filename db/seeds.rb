7bc5967043e3d00642eac0de1586cc399b0d2995
SnowSurfer <drinkaziu@gmail.com>
2018-05-09 16:15:07 -0400

    soft MVP complete - incomplete seeding

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+
+drin = User.create(name: "Drin")
+faraz = User.create(name: "Faraz")
+
+
+basketball = Sport.create(name: "Basketball")
+baseball = Sport.create(name: "Baseball")
+
+johns_subs = Subscription.create(name: "John's Subscription")
